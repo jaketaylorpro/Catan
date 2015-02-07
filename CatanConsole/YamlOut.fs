5cc066f7f7f1e421c08ae3d823ef26c3164baf84
Jake Taylor <JakeTaylorPro@gmail.com>
2015-02-06 22:41:51 -0500

    got yaml serializer test working

--- CatanConsole/YamlOut.fs
+++ CatanConsole/YamlOut.fs
@@ -1,21 +1,25 @@
 ﻿module YamlOut
 open Microsoft.FSharp.Reflection
 let toYaml (o:obj) :string =
-    let rec toYamli (o:obj) (ind:int) :string =
+    let rec toYamli (o) (ind:int) :string =
         let rec tabs n=
             match n with
             | 0 -> ""
             | _ -> "  "+(tabs (n-1))
         let arrayToYaml l (ind:int)=
-            Array.fold (fun acc i->acc+(tabs ind)+"- "+(toYamli i (ind+1))+"\n") "\n" l 
+            "(l)"+Array.fold (fun acc i->acc+(tabs ind)+"- "+(toYamli i (ind+1))+"\n") "\n" l 
         let recordToYaml r (ind:int) =
-            Array.fold (fun (acc:string) (pi:System.Reflection.PropertyInfo)-> acc+(tabs ind)+pi.Name+": "+(toYamli (pi.GetMethod.Invoke(r,[]|>List.toArray)) (ind+1))+"\n") "\n" (FSharpType.GetRecordFields(r.GetType()))
-        let unionToYaml u (ind:int) =
-            (tabs ind)+u.GetType.ToString()+"."+u.ToString()//handle parameterized unions SOME(x)
+            "(r)"+Array.fold (fun (acc:string) (pi:System.Reflection.PropertyInfo)-> acc+(tabs ind)+pi.Name+": "+(toYamli (pi.GetMethod.Invoke(r,[]|>List.toArray)) (ind+1))+"\n") "\n" (FSharpType.GetRecordFields(r.GetType()))
+        let classToYaml c (ind:int) =
+            "(c)"+Array.fold (fun (acc:string) (pi:System.Reflection.PropertyInfo)-> acc+(tabs ind)+pi.Name+": "+(toYamli (pi.GetMethod.Invoke(c,[]|>List.toArray)) (ind+1))+"\n") "\n" (c.GetType().GetProperties())
+        let unionToYaml (u:obj) (ind:int) =
+            //(tabs ind)+"(u)"+(FSharpType.GetUnionCases(u.GetType())|>(Array.find (fun (ucase:UnionCaseInfo) -> ucase. = (Microsoft.FSharp.Core.LanguagePrimitives. u)))).Name
+            //(tabs ind)+"(u)"+Array.fold (fun (acc:string) (ui:UnionCaseInfo)->acc+ui.Name+","+ui.Tag.ToString()+";") "" (FSharpType.GetUnionCases(u.GetType()))
+            (tabs ind)+"(u)"+u.GetType().Name+"."+(sprintf "%A" u)
         let tupleToYaml t (ind:int) =
-            (tabs ind)+t.ToString()
+            (tabs ind)+"(t)"+t.ToString()
         let functionToYaml t (ind:int) =
-            (tabs ind)+t.ToString()
+            (tabs ind)+"(f)"+t.ToString()
         let t=o.GetType
         match o with
         | a when a.GetType().IsArray -> arrayToYaml (a:?>array<_>) ind
@@ -23,5 +27,9 @@ let toYaml (o:obj) :string =
         | u when FSharpType.IsUnion (u.GetType()) -> unionToYaml u ind
         | t when FSharpType.IsTuple (t.GetType()) -> tupleToYaml t ind
         | f when FSharpType.IsFunction (f.GetType()) -> functionToYaml f ind
-        | x -> x.ToString()
+        | p when p.GetType().IsPrimitive -> "(p)"+p.ToString()
+        | c when c.GetType().IsClass -> classToYaml c ind
+        | v when v.GetType().IsValueType -> "(v)"+v.ToString()
+        | e when e.GetType().IsEnum -> "(e)"+e.ToString()
+        | x -> "(x)"+x.ToString()
     toYamli o 0
